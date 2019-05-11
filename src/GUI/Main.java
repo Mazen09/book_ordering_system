612d6a49591e3243f83bf82c27211dc933d06514
MohamedEsmailMohamed <mesmail65525@gmail.com>
2019-05-11 16:47:14 +0200

    fix cart problem after check out

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -538,7 +538,7 @@ public class Main extends Application {
             @Override
             public void handle(ActionEvent event) {
                 setCartStageActions();
-
+                cartStage.itemsPane.getChildren().clear();
                 ArrayList<CartItem> cartItems = null;
                 try {
                     cartItems = backEnd.getCartContent(user.userName);
